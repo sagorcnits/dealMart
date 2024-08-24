01ea28863e35f51989a0bfcdcc5758b5e07708ae
Sagor Hossain <sagor.cnits@gmail.com>
2024-08-24 12:46:42 +0600

    contact page added

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    );
+  return (
+    <main className="max-w-7xl mx-auto px-2">
+      <div className="mt-6 h-[220px] md:h-[350px] bg-[url('https://img.freepik.com/free-photo/contact-us-communication-support-service-assistance-concept_53876-128103.jpg?t=st=1724481470~exp=1724485070~hmac=5d3f14a363d5a34bb9ee2fca15749baeb8847f6dcec39a1cf2b00ab55b0038d8&w=900')] bg-no-repeat bg-cover bg-center rounded-md">
+      </div>
+      <section>
+         <h1>Our Address</h1>
+         <div>
+             <div>
+                  
+             </div>
+         </div>
+      </section>
+    </main>
+  );
 };
 
 export default Contact;
