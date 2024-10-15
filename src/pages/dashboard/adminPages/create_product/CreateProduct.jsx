de192285cf328e55be186f51d8012b7b8833d2c1
Sagor Hossain <sagor.cnits@gmail.com>
2024-10-15 12:30:27 +0600

    solve upload img on the cloudinary

--- src/pages/dashboard/adminPages/create_product/CreateProduct.jsx
+++ src/pages/dashboard/adminPages/create_product/CreateProduct.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { BiImage } from "react-icons/bi";
+import { IoCloseSharp } from "react-icons/io5";
 const CreateProduct = () => {
+  const [images, setImages] = useState([]);
+  const [imageId, setImageId] = useState([]);
   const {
     register,
     handleSubmit,
@@ -19,10 +23,6 @@ const CreateProduct = () => {
     const quantity_in_stock = data.quantity_in_stock;
     const stock_status = data.stock_status;
     const description = data.description;
-    const img1 = data.img1;
-    const img2 = data.img2;
-    const img3 = data.img3;
-    const img4 = data.img4;
 
     const productData = {
       product_name,
@@ -33,14 +33,62 @@ const CreateProduct = () => {
       quantity_in_stock,
       stock_status,
       description,
-      img1,
-      img2,
-      img3,
-      img4,
+      images: images,
     };
+
+    if (images.length > 2) {
+      return alert("Please Provide More Product Image");
+    }
 
     console.log(productData);
   };
+  // images upload method in cloundinary
+  const handleFileChange = (event) => {
+    console.log("ok");
+
+    const formData = new FormData();
+    formData.append("file", event.target.files[0]);
+    formData.append("upload_preset", "dealMart");
+    formData.append("api_key", "941311292871449");
+
+    axios
+      .post("https://api.cloudinary.com/v1_1/dqsqzp3an/image/upload", formData)
+      .then((response) => {
+        if (response.data.asset_id) {
+          setImages((prev) => [...prev, response.data.secure_url]);
+          setImageId((prev) => [
+            ...prev,
+            {
+              public_id: response.data.public_id,
+              imgUrl: response.data.secure_url,
+            },
+          ]);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  //  delete images
+  const removeProductImage = (imgLength, public_id) => {
+    console.log(imgLength, public_id);
+
+    axios.delete(
+      `https://api.cloudinary.com/v1_1/dqsqzp3an/resources/image/upload/${public_id}`,
+      {
+        auth: {
+          username: "941311292871449",
+          password: "cDFHwNBnkc6MORcbxTNAJ7bs_d8",
+        },
+      }
+    );
+
+    const removeImages = imageId.filter((item, id) => id !== imgLength);
+    setImageId(removeImages);
+  };
+
+  // console.log(imageId);
 
   return (
     <main className="mt-16">
@@ -190,70 +238,151 @@ const CreateProduct = () => {
             <label className="block mb-1 text-xs poppins font-bold text-paragraph">
               Product Image
             </label>
-            <div className="grid grid-cols-4 gap-2">
-              <div className="border border-blue border-dashed p-2 text-center flex justify-center items-center">
-                <label
-                  htmlFor="fileUpload"
-                  className="cursor-pointer text-green poppins text-sm"
-                >
-                  <BiImage className="mx-auto" size={20}></BiImage>
-                  Browse img
-                </label>
-                <input
-                  {...register("img1", { required: true })}
-                  type="file"
-                  id="fileUpload"
-                  className="hidden"
-                />
-                {errors.img1 && <p className="text-red-500">Invalid</p>}
+            <div className="grid grid-cols-4 gap-2 *:border *:border-blue *:border-dashed *:p-2 *:text-center *:flex *:justify-center *:items-center  *:h-[200px] *:overflow-hidden *:relative">
+              <div>
+                {imageId.length == 0 ? (
+                  <>
+                    <label
+                      htmlFor="fileUpload"
+                      className="cursor-pointer text-green poppins text-sm"
+                    >
+                      <BiImage className="mx-auto" size={20}></BiImage>
+                      Browse img
+                    </label>
+                    <input
+                      onChange={handleFileChange}
+                      type="file"
+                      id="fileUpload"
+                      className="hidden"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <img
+                      className="w-full h-full object-cover"
+                      src={imageId[0]?.imgUrl}
+                      alt="img1"
+                    />
+                    <div
+                      onClick={() =>
+                        removeProductImage(0, imageId[0]?.public_id)
+                      }
+                      className="absolute top-2 right-2 size-6 rounded-full bg-red-500 text-white hover:bg-blue duration-500 cursor-pointer flex justify-center items-center"
+                    >
+                      <IoCloseSharp></IoCloseSharp>
+                    </div>
+                  </>
+                )}
               </div>
-              <div className="border border-blue border-dashed p-2 text-center flex justify-center items-center">
-                <label
-                  htmlFor="fileUpload2"
-                  className="cursor-pointer text-green poppins text-sm"
-                >
-                  <BiImage className="mx-auto" size={20}></BiImage>
-                  Browse img
-                </label>
-                <input
-                  {...register("img2", { required: true })}
-                  type="file"
-                  id="fileUpload2"
-                  className="hidden"
-                />
-                {errors.img2 && <p className="text-red-500">Invalid</p>}
+              <div>
+                {imageId.length == 1 || imageId.length == 0 ? (
+                  <>
+                    <label
+                      htmlFor="fileUpload2"
+                      className="cursor-pointer text-green poppins text-sm"
+                    >
+                      <BiImage className="mx-auto" size={20}></BiImage>
+                      Browse img
+                    </label>
+                    <input
+                      onChange={handleFileChange}
+                      type="file"
+                      id="fileUpload2"
+                      className="hidden"
+                    />{" "}
+                  </>
+                ) : (
+                  <>
+                    <img
+                      className="w-full h-full object-cover"
+                      src={imageId[1]?.imgUrl}
+                      alt="img2"
+                    />
+                    <div
+                      onClick={() =>
+                        removeProductImage(1, imageId[1]?.public_id)
+                      }
+                      className="absolute top-2 right-2 size-6 rounded-full bg-red-500 text-white hover:bg-blue duration-500 cursor-pointer flex justify-center items-center"
+                    >
+                      <IoCloseSharp></IoCloseSharp>
+                    </div>
+                  </>
+                )}
               </div>
-              <div className="border border-blue border-dashed p-2 text-center flex justify-center items-center">
-                <label
-                  htmlFor="fileUpload3"
-                  className="cursor-pointer text-green poppins text-sm"
-                >
-                  <BiImage className="mx-auto" size={20}></BiImage>
-                  Browse img
-                </label>
-                <input
-                  {...register("img3", { required: true })}
-                  type="file"
-                  id="fileUpload3"
-                  className="hidden"
-                />
-                {errors.img3 && <p className="text-red-500">Invalid</p>}
+              <div>
+                {imageId.length == 0 ||
+                imageId.length == 1 ||
+                imageId.length == 2 ? (
+                  <>
+                    <label
+                      htmlFor="fileUpload3"
+                      className="cursor-pointer text-green poppins text-sm"
+                    >
+                      <BiImage className="mx-auto" size={20}></BiImage>
+                      Browse img
+                    </label>
+                    <input
+                      onChange={handleFileChange}
+                      type="file"
+                      id="fileUpload3"
+                      className="hidden"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <img
+                      className="w-full h-full object-cover"
+                      src={imageId[2]?.imgUrl}
+                      alt="img2"
+                    />
+                    <div
+                      onClick={() =>
+                        removeProductImage(2, imageId[2]?.public_id)
+                      }
+                      className="absolute top-2 right-2 size-6 rounded-full bg-red-500 text-white hover:bg-blue duration-500 cursor-pointer flex justify-center items-center"
+                    >
+                      <IoCloseSharp></IoCloseSharp>
+                    </div>
+                  </>
+                )}
               </div>
-              <div className="border border-blue border-dashed p-2 text-center flex justify-center items-center">
-                <label
-                  htmlFor="fileUpload4"
-                  className="cursor-pointer text-green poppins text-sm"
-                >
-                  <BiImage className="mx-auto" size={20}></BiImage>
-                  Browse img
-                </label>
-                <input
-                  {...register("img4", { required: true })}
-                  type="file"
-                  id="fileUpload4"
-                  className="hidden"
-                />
-                {errors.img4 && <p className="text-red-500">Invalid</p>}
+              <div>
+                {imageId.length == 0 ||
+                imageId.length == 1 ||
+                imageId.length == 2 ||
+                imageId.length == 3 ? (
+                  <>
+                    <label
+                      htmlFor="fileUpload4"
+                      className="cursor-pointer text-green poppins text-sm"
+                    >
+                      <BiImage className="mx-auto" size={20}></BiImage>
+                      Browse img
+                    </label>
+                    <input
+                      onChange={handleFileChange}
+                      type="file"
+                      id="fileUpload4"
+                      className="hidden"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <img
+                      className="w-full h-full object-cover"
+                      src={imageId[3]?.imgUrl}
+                      alt="img3"
+                    />
+                    <div
+                      onClick={() =>
+                        removeProductImage(3, imageId[3]?.public_id)
+                      }
+                      className="absolute top-2 right-2 size-6 rounded-full bg-red-500 text-white hover:bg-blue duration-500 cursor-pointer flex justify-center items-center"
+                    >
+                      <IoCloseSharp></IoCloseSharp>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           </div>
