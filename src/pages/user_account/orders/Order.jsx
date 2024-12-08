2ba09bc877a7c38d60795dfbc973183f4494e73e
Sagor Hossain <130352065+sagorcnits@users.noreply.github.com>
2024-12-08 12:55:31 +0600

    customer er order table added

--- src/pages/user_account/orders/Order.jsx
+++ src/pages/user_account/orders/Order.jsx
@@ -1,34 +1,121 @@
+import { useEffect, useState } from "react";
+import { IoEyeOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import useAxios from "../../../hooks/useAxios";
+
 const Order = () => {
+  const axiosFetch = useAxios();
+  const user = useSelector((state) => state.user.user);
+  const [loading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
+  // fetch order
+  useEffect(() => {
+    console.log("ok");
+    if (user?.email) {
+      axiosFetch
+        .get(`/orders/email/${user?.email}`)
+        .then((res) => {
+          setOrders(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  }, [user]);
+  // loading
+  if (loading) {
+    return (
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue mx-auto"></div>
+    );
+  }
+
+  // console.log(orders)
   return (
-    <table className="w-full">
-      <thead>
-        <tr className="text-left *:p-3 border-b *:uppercase">
-          <th>#Order Id</th>
-          <th>Date</th>
-          <th>Total</th>
-          <th>Items</th>
-          <th>Payment Status</th>
-          <th>Order Status</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {[1, 2, 3, 4, 5, 6].map((item, id) => {
-          return (
-            <tr key={id}
-            className="*:p-3 border-b items-center hover:bg-[#f1efef] duration-500 *:text-gray-700">
-              <td>#3434343434</td>
-              <td>new</td>
-              <td>new</td>
-              <td>new</td>
-              <td>new</td>
-              <td>new</td>
-              <td>new</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <h1 className="p-2 font-semibold text-2xl">Your Orders</h1>
+      <table className="w-full">
+        <thead>
+          <tr className="text-left *:p-3 border-b *:uppercase">
+            <th>#Order Id</th>
+            <th>Date</th>
+            <th>Total</th>
+            <th>Items</th>
+            <th>Payment Status</th>
+            <th>Order Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders?.order?.map((item, id) => {
+            const {
+              _id,
+              orderId,
+              products,
+              customer,
+              total_price,
+              payment_status,
+              order_status,
+              createdAt,
+            } = item;
+            return (
+              <tr
+                key={id}
+                className="*:p-3 border-b items-center hover:bg-[#f1efef] duration-500 *:text-gray-700"
+              >
+                <td>{orderId}</td>
+                <td>{createdAt.slice(0, 10)}</td>
+                <td>${total_price}</td>
+                <td>{products.length}</td>
+                <td>
+                  {" "}
+                  <p
+                    className={`${
+                      payment_status == "paid"
+                        ? "text-green"
+                        : payment_status == "refund"
+                        ? "text-customRed"
+                        : ""
+                    } font-semibold`}
+                  >
+                    {payment_status}
+                  </p>
+                </td>
+                <td>
+                  {" "}
+                  <p
+                    className={`${
+                      order_status?.status === "complated" &&
+                      payment_status == "paid"
+                        ? "text-green"
+                        : order_status?.status === "courier"
+                        ? "text-lime-700"
+                        : order_status?.status === "progress"
+                        ? "text-darkBlue"
+                        : order_status?.status === "canceled"
+                        ? "text-customRed"
+                        : payment_status == "refund"
+                        ? "text-customRed"
+                        : ""
+                    } font-semibold`}
+                  >
+                    {payment_status == "refund"
+                      ? "canceled"
+                      : order_status?.status}
+                  </p>
+                </td>
+                <td>
+                  <Link to={`/dashboard/order-details/${_id}`}>
+                    <IoEyeOutline size={20}></IoEyeOutline>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
