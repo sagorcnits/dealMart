af068e783f9403cc648516393954662f5fea8d12
Sagor Hossain <130352065+sagorcnits@users.noreply.github.com>
2025-02-01 22:15:34 +0600

    socket link shnages

--- src/components/Socket_provider.jsx
+++ src/components/Socket_provider.jsx
@@ -11,7 +11,7 @@ const Socket_Provider = ({ children }) => {
     const user_email = localStorage.getItem("user_email");
     const axiosPublic = useAxios()
     useEffect(() => {
-        const socket = io("http://localhost:5000/");
+        const socket = io("https://dealmart-server-wxsp.vercel.app/");
         socket.on("connect", () => {
             setNewSocket(socket);
             console.log(socket)
